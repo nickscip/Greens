7217337f26f8afa8762b8d6fffba39658109500d
Nickscip <nickscip@gmail.com>
2022-01-17 23:05:38 -0600

    Sleep

--- App.js
+++ App.js
@@ -1,32 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button, Pressable, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>SMART APP</Text>
-      <StatusBar style="auto" />
-      <View style={styles.buttonContainer}>
-        <Button title = "Find Art" />
-      </View>
+    <View>
+      <Pressable>
+        <Text style={styles.text}>Find Art</Text>
+      </Pressable>
     </View>
-    
   );
 }
 
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SMART APP" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  homeButton: {
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    padding: 50,
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'blue',
   },
   text: {
-    fontSize: 50
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
-  buttonContainer: {
-    margin: 30,
-    alignItems: 'flex-start'
-  }
 });
